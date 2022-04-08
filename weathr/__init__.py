4b0d6d5dafd9a7c3724e1692ad33286ba69412e1
Daniel Del Percio <daniel.t.delpercio@gmail.com>
2022-04-08 14:34:52 +0200

    First working prototype

--- weathr/__init__.py
+++ weathr/__init__.py
@@ -6,10 +6,6 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -23,12 +19,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
-    from . import db
-    db.init_app(app)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from . import weather
+    app.register_blueprint(weather.bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
---
+++ weathr/weather.py
@@ -0,0 +1,30 @@
+import requests
+
+from flask import(
+    Blueprint,render_template,request
+)
+
+bp = Blueprint('weather',__name__)
+
+@bp.route('/', methods=['GET','POST'])
+def index():
+    city = 'Las Vegas'
+    
+    if request.method == 'POST':
+        city = request.form.get('city')
+
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=1dbaf7baf220c461d9be1a59aebba09a'
+    
+
+    r = requests.get(url.format(city)).json()
+    
+
+    weather = {
+        'city' : city,
+        'temp' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+    }
+
+    print(weather)
+
+    return render_template('index.html',weather=weather)
